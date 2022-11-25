83088156f7fcc7cfd489beed6a889b686159309a
Rob Vandelinder <robjvan@gmail.com>
2022-11-25 16:44:23 -0330

    Built basic form without styling

--- lib/pages/new_book_screen/new_book_form.dart
+++ lib/pages/new_book_screen/new_book_form.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:librarian_mobile/api/books_api.dart';
+import 'package:librarian_mobile/models/book.model.dart';
+import 'package:librarian_mobile/pages/library_screen/library_screen.dart';
+import 'package:librarian_mobile/widgets/library_fab.widget.dart';
 
 class NewBookForm extends StatefulWidget {
   const NewBookForm({super.key});
@@ -11,47 +15,284 @@ class NewBookForm extends StatefulWidget {
 
 class _NewBookFormState extends State<NewBookForm> {
   final TextEditingController titleController = TextEditingController();
+  final TextEditingController authorController = TextEditingController();
   final TextEditingController descriptionController = TextEditingController();
+  final TextEditingController pageCountController = TextEditingController();
+  final TextEditingController publishYearController = TextEditingController();
+  final TextEditingController isbn10Controller = TextEditingController();
+  final TextEditingController isbn13Controller = TextEditingController();
+
+  late bool isMature = false;
+  late bool haveRead = false;
+  late bool inShoppingList = false;
+  late bool inFavesList = false;
+  late bool inWishlist = false;
 
   @override
   void dispose() {
-    super.dispose();
     titleController.dispose();
     descriptionController.dispose();
+    pageCountController.dispose();
+    publishYearController.dispose();
+    isbn10Controller.dispose();
+    isbn13Controller.dispose();
+    super.dispose();
   }
 
-  List<Widget> _buildChildren() {
-    var children = <Text>[];
-    for (int i = 0; i < 100; i++) {
-      children.add(Text('text ${i + 1}'));
-    }
-    return children;
+  void submitFunction() {
+    // Grab values from various fields, send off to API for processing
+    Book newBook = Book(
+        title: titleController.text,
+        description: descriptionController.text,
+        pageCount: int.parse(pageCountController.text),
+        publishYear: int.parse(publishYearController.text),
+        author: authorController.text,
+        haveRead: haveRead,
+        inFavesList: inFavesList,
+        inShoppingList: inShoppingList,
+        inWishlist: inWishlist,
+        isMature: isMature,
+        isbn10: int.parse(isbn10Controller.text),
+        isbn13: int.parse(isbn13Controller.text),
+        publisher: publishYearController.text);
+    BooksApi.addNewBook(newBook);
   }
 
   @override
   Widget build(BuildContext context) {
-    final double sw = MediaQuery.of(context).size.width;
-
     return Form(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          SizedBox(
-            width: sw,
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Text('Title:'),
-                TextFormField(
-                  maxLength: 32,
-                  // decoration: InputDecoration(
-                  //   hintText: 'userName',
-                  // ),
-                ),
-              ],
-            ),
+          _buildPhotoPicker(),
+          _buildTitleField(),
+          _buildAuthorField(),
+          _buildDescriptionField(),
+          _buildPageCountField(),
+          _buildPublishYearField(),
+          _buildCheckboxFields(),
+          _buildIsbnFields(),
+          _buildSubmitButton(),
+          _buildCancelButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPhotoPicker() {
+    // TODO(Rob): Add ability to add an image from camera/gallery
+    return Container(
+      padding: const EdgeInsets.only(bottom: 8),
+      height: 200,
+      width: 130,
+      child: Container(
+        decoration: BoxDecoration(border: Border.all()),
+      ),
+      // TODO(Rob): Add placeholder image
+    );
+  }
+
+  Widget _buildTitleField() {
+    return _genericInputField(
+      label: 'Title:',
+      hintText: '"Jurassic Park"',
+      controller: titleController,
+    );
+  }
+
+  Widget _buildAuthorField() {
+    return _genericInputField(
+      label: 'Author:',
+      hintText: '"Michael Crichton"',
+      controller: authorController,
+    );
+  }
+
+  Widget _buildDescriptionField() {
+    return _genericInputField(
+      label: 'Description:',
+      hintText: '"A book about dinosaurs"',
+      controller: descriptionController,
+    );
+  }
+
+  Widget _buildPageCountField() {
+    return _genericInputField(
+      label: 'Page Count:',
+      hintText: '"328"',
+      controller: pageCountController,
+    );
+  }
+
+  Widget _buildPublishYearField() {
+    return _genericInputField(
+      label: 'Publish Year:',
+      hintText: '"1994"',
+      controller: publishYearController,
+    );
+  }
+
+  Widget _buildIsbnFields() {
+    return Column(
+      children: [
+        _genericInputField(
+          label: 'ISBN10:',
+          hintText: '"1234567890"',
+          controller: isbn10Controller,
+        ),
+        _genericInputField(
+          label: 'ISBN13:',
+          hintText: '"1234567890123"',
+          controller: isbn13Controller,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCheckboxFields() {
+    // haveRead, inWishList, inFavesList, inShoppingList, isMature
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              _genericCheckboxField('haveread', label: 'Have read before?'),
+              _genericCheckboxField('faves',
+                  label: 'Add to Faves?', checked: inFavesList),
+            ],
+          ),
+          Row(
+            children: [
+              _genericCheckboxField('wishlist', label: 'Add to Wishlist?'),
+              _genericCheckboxField('shoppinglist',
+                  label: 'Add to Shopping list?'),
+            ],
+          ),
+          Row(
+            children: [
+              _genericCheckboxField('ismature',
+                  label: 'Contains mature content?'),
+            ],
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _genericInputField({
+    required String label,
+    required String hintText,
+    required TextEditingController controller,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: SizedBox(
+        width: Get.width,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              width: 150,
+              child: Text(label),
+            ),
+            const SizedBox(width: 8),
+            SizedBox(
+              width: (Get.width) * 0.6,
+              child: TextFormField(
+                onChanged: (newValue) {
+                  print('newValue = $newValue');
+                  print('controller.text = ${controller.text}');
+                },
+                controller: controller,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: hintText,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _genericCheckboxField(
+    String purpose, {
+    required String label,
+    bool checked = false,
+  }) {
+    return SizedBox(
+      width: Get.width / 2,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(
+            width: 175,
+            child: Text(label),
+          ),
+          const SizedBox(width: 8),
+          SizedBox(
+              child: Checkbox(
+            value: checked,
+            onChanged: (val) {
+              setState(() {
+                switch (purpose) {
+                  case 'faves':
+                    inFavesList = !inFavesList;
+                    break;
+                  case 'haveread':
+                    haveRead = !haveRead;
+                    break;
+                  case 'wishlist':
+                    inWishlist = !inWishlist;
+                    break;
+                  case 'shoppinglist':
+                    inShoppingList = !inShoppingList;
+                    break;
+                  case 'ismature':
+                    isMature = !isMature;
+                    break;
+                }
+              });
+            },
+          )),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSubmitButton() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size(200, 60),
+          maximumSize: const Size(200, 60),
+        ),
+        onPressed: submitFunction,
+        child: const Text(
+          'Submit',
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCancelButton() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: TextButton(
+        style: TextButton.styleFrom(
+          minimumSize: const Size(200, 40),
+          maximumSize: const Size(200, 40),
+        ),
+        onPressed: () {
+          Get.offAll(const LibraryScreen());
+        },
+        child: const Text('Cancel'),
       ),
     );
   }
