ae0a32566bd0035c8a6ec97b0ccc5a34452b1943
Rob Vandelinder <robjvan@gmail.com>
2022-11-25 16:43:53 -0330

    Added placeholder for addNewBook

--- lib/api/books_api.dart
+++ lib/api/books_api.dart
@@ -1,7 +1,18 @@
 import 'package:get/get.dart';
 import 'package:librarian_mobile/controllers/user_data.controller.dart';
+import 'package:librarian_mobile/models/book.model.dart';
 
 class BooksApi {
   // final BooksAPIProvider _booksAPIProvider = BooksAPIProvider();
   final UserDataController _booksController = Get.put(UserDataController());
+
+  static void addNewBook(final Book book) {
+    //* Use passed book info to generate an entry for the db
+    //* - Use Title info to find titleId (or create new entry)
+    //* - Use Author info to find authorId (or create new entry)
+    //* - Use publisher info to find pushlisherId (or create new entry)
+    //* - Use publishYear info to find pushlishYearId (or create new entry)
+    //* - Backend will attach userId
+    print(book.title);
+  }
 }
