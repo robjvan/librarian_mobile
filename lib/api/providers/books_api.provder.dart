27fe8becdd0d9b9934e2f5f6bcfa5056f82dad1b
Rob Vandelinder <robjvan@gmail.com>
2022-11-25 17:22:38 -0330

    Added saveNewBook method

---
+++ lib/api/providers/books_api.provder.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:librarian_mobile/config/config.dart' as config;
+import 'package:librarian_mobile/models/book.model.dart';
+
+@immutable
+class BooksAPIProvider {
+  final String _apiUrl = '/books';
+
+  // // Get titleId from DB (backend will create if needed)
+  // Future<void> getTitleId(final String title) async {
+  //   // TODO(Rob): Add logic to grab title ID from backend
+
+  //   return await null;
+  // }
+
+  // // Get authorId from DB
+  // Future<void> getAuthorId(final String author) async {
+  //   // TODO(Rob): Add logic to grab title ID from backend
+
+  //   return await null;
+  // }
+
+  // // Get descriptionId from DB
+  // Future<void> getDescriptionId(final String author) async {
+  //   // TODO(Rob): Add logic to grab title ID from backend
+
+  //   return await null;
+  // }
+
+  // // Get publisherId from DB
+  // Future<void> getPublisherId(final String publisher) async {
+  //   // TODO(Rob): Add logic to grab title ID from backend
+
+  //   return await null;
+  // }
+
+  // // Get publishYearId from DB
+  // Future<void> getPublishYearId(final int year) async {
+  //   // TODO(Rob): Add logic to grab title ID from backend
+
+  //   return await null;
+  // }
+
+  // // Get isbn10 from DB
+  // Future<void> getIsbn10Id(final int year) async {
+  //   // TODO(Rob): Add logic to grab title ID from backend
+
+  //   return await null;
+  // }
+
+  // // Get isbn13 from DB
+  // Future<void> getIsbn13Id(final int year) async {
+  //   // TODO(Rob): Add logic to grab title ID from backend
+
+  //   return await null;
+  // }
+
+  // save new book to DB
+  Future<http.Response> saveNewBook(final Book book) {
+    final Uri saveUrl = config.getAPIUrl().resolve('$_apiUrl/books');
+
+    return http.post(
+      saveUrl,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: book.toJson(),
+    );
+  }
+}
