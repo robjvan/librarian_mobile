e2ff20e3ffe785fc56ff82d7a50206c047cad422
Rob Vandelinder <robjvan@gmail.com>
2022-11-26 21:00:50 -0330

    Add call to API provider

--- lib/api/books_api.dart
+++ lib/api/books_api.dart
@@ -1,27 +1,42 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:librarian_mobile/api/providers/books_api.provder.dart';
 import 'package:librarian_mobile/controllers/user_data.controller.dart';
 import 'package:librarian_mobile/models/book.model.dart';
+import 'package:http/http.dart' as http;
 
 class BooksApi {
   final BooksAPIProvider _booksAPIProvider = BooksAPIProvider();
   final UserDataController _booksController = Get.put(UserDataController());
 
-  void addNewBook(final Book book) {
+  void addNewBook(final Book book) async {
+    late http.Response? response;
     //* Use passed book info to generate an entry for the db
-    //* - Use Title info to find titleId (or create new entry)
-    //* - Use Author info to find authorId (or create new entry)
-    //* - Use publisher info to find pushlisherId (or create new entry)
-    //* - Use publishYear info to find pushlishYearId (or create new entry)
-    //* - Backend will attach userId
-    print(jsonEncode(book));
+    //! The backend requires this data to be in the structure of a CreateBookDto
+    // Required field: title
+    // Optional: description, pageCount, author, publisher, pubhlishYear,
+    // haveRead, isMature, inWishlist, inShoppingList, inFavesList, isbn10,
+    // isbn13, rating, thumbnailUrl
+    // print(jsonEncode(book));
 
     try {
-      // _booksAPIProvider.saveNewBook(book);
+      response = await _booksAPIProvider.saveNewBook(book);
     } on Exception catch (err) {
       print(err);
     }
+
+    if (response != null) {
+      debugPrint(response.body);
+    }
+
+    // TODO: Add error handling:
+    // 400 - bad request, etc
+
+    // if (response.statusCode != 201) {}
+    // if (result) {
+    //   return result;
+    // }
   }
 }
