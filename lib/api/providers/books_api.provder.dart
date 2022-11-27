f81973cb68553ba91feeba2e28b9fc4cf1905c1c
Rob Vandelinder <robjvan@gmail.com>
2022-11-26 21:01:27 -0330

    Added auth header

--- lib/api/providers/books_api.provder.dart
+++ lib/api/providers/books_api.provder.dart
@@ -1,13 +1,16 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'package:librarian_mobile/config/config.dart' as config;
+import 'package:librarian_mobile/controllers/auth.controller.dart';
 import 'package:librarian_mobile/models/book.model.dart';
 
 @immutable
 class BooksAPIProvider {
-  final String _apiUrl = '/books';
+  final String _apiUrl = 'api/v1/books';
+  final AuthController authController = Get.put(AuthController());
 
   // // Get titleId from DB (backend will create if needed)
   // Future<void> getTitleId(final String title) async {
@@ -60,14 +63,16 @@ class BooksAPIProvider {
 
   // save new book to DB
   Future<http.Response> saveNewBook(final Book book) {
-    final Uri saveUrl = config.getAPIUrl().resolve('$_apiUrl/books');
+    final Uri saveUrl = config.getAPIUrl().resolve('$_apiUrl');
+    final String token = authController.authToken;
 
     return http.post(
       saveUrl,
       headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
       },
-      body: book.toJson(),
+      body: jsonEncode(book.toCreateBookDto()),
     );
   }
 }
