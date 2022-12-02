138aafd64401bd219a164030438020d22cab7a29
Rob Vandelinder <robjvan@gmail.com>
2022-12-02 09:01:04 -0330

    Added fetchBooks

--- lib/api/books_api.dart
+++ lib/api/books_api.dart
@@ -3,14 +3,14 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:librarian_mobile/api/providers/books_api.provder.dart';
-import 'package:librarian_mobile/controllers/user_data.controller.dart';
+import 'package:librarian_mobile/controllers/books.controller.dart';
 import 'package:librarian_mobile/models/book.model.dart';
 import 'package:http/http.dart' as http;
 import 'package:librarian_mobile/pages/library_screen/library_screen.dart';
 
 class BooksApi {
   final BooksAPIProvider _booksAPIProvider = BooksAPIProvider();
-  final UserDataController _booksController = Get.put(UserDataController());
+  final BooksController _booksController = Get.put(BooksController());
 
   void addNewBook(final Book book) async {
     late http.Response? response;
@@ -25,18 +25,45 @@ class BooksApi {
     try {
       response = await _booksAPIProvider.saveNewBook(book);
     } on Exception catch (err) {
-      print(err);
+      debugPrint('$err');
     }
 
     if (response != null) {
       debugPrint(response.body);
+      // TODO: Add error handling:
+      // 400 - bad request, etc
+
+      if (response.statusCode == 201) {
+        // Status 201 means record was successfully added to DB
+        // Add the book to local array
+        final BooksController booksCtlr = BooksController();
+        booksCtlr.addBook(book);
+        Get.offAll(const LibraryScreen());
+      }
+    } else {
+      /// Could not save new book to DB, throw error and investigate reason
+    }
+  }
+
+  Future<void> fetchBooks() async {
+    late http.Response? response;
+
+    /// 1. grab the list of books from the backend
+    try {
+      response = await _booksAPIProvider.fetchBooks();
+      print(response.body);
+      if (response != null) {
+        /// 2. process into local storage
+        List<dynamic> jsonResponse = json.decode(response.body);
+        jsonResponse.forEach((book) {
+          _booksController.addBook(Book.fromDb(book));
+        });
+        // String test = response.body;
+        // Book.fromDb(test);
+    }
+    } on Exception catch (err) {
+      debugPrint('Could not fetch books from server: $err');
     }
 
-    // TODO: Add error handling:
-    // 400 - bad request, etc
-
-    if (response!.statusCode == 201) {
-      Get.offAll(LibraryScreen());
-    }
   }
 }
